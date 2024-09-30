60d454723cda1cff883d960198bd473e41c3d83b
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-09-30 17:37:41 +0530

    hhh

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['trb9yrhq5p76ro9s.public.blob.vercel-storage.com', 'avatar.vercel.sh'],
   },
-  output: 'export',
   basePath: '/Shehzaan',
 };
 
