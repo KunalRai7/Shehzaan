6b69b22d214c2577a9641e06515ee28b7c28cda9
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-09-30 17:50:53 +0530

    img

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,47 +1,54 @@
 import { motion } from 'framer-motion'
-import CustomImage from './CustomImage'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 const interiorProjects = [
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4659.JPG', alt: 'Interior Project 1' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5169.JPG', alt: 'Interior Project 2' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5170.JPG', alt: 'Interior Project 3' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5171.JPG', alt: 'Interior Project 4' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5172.JPG', alt: 'Interior Project 5' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5593.JPG', alt: 'Interior Project 6' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5594.JPG', alt: 'Interior Project 7' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5595.JPG', alt: 'Interior Project 8' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5661.JPG', alt: 'Interior Project 9' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5662.JPG', alt: 'Interior Project 10' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5663.JPG', alt: 'Interior Project 11' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_5664.JPG', alt: 'Interior Project 12' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_3810.JPG', alt: 'Interior Project 13' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_3811.JPG', alt: 'Interior Project 14' },
+  { src: '/images/interior/IMG_4659.jpg', alt: 'Interior Project 1' },
+  { src: '/images/interior/IMG_5169.jpg', alt: 'Interior Project 2' },
+  { src: '/images/interior/IMG_5170.jpg', alt: 'Interior Project 3' },
+  { src: '/images/interior/IMG_5171.jpg', alt: 'Interior Project 4' },
+  { src: '/images/interior/IMG_5172.jpg', alt: 'Interior Project 5' },
+  { src: '/images/interior/IMG_5593.jpg', alt: 'Interior Project 6' },
+  { src: '/images/interior/IMG_5594.jpg', alt: 'Interior Project 7' },
+  { src: '/images/interior/IMG_5595.jpg', alt: 'Interior Project 8' },
+  { src: '/images/interior/IMG_5661.jpg', alt: 'Interior Project 9' },
+  { src: '/images/interior/IMG_5662.jpg', alt: 'Interior Project 10' },
+  { src: '/images/interior/IMG_5663.jpg', alt: 'Interior Project 11' },
+  { src: '/images/interior/IMG_5664.jpg', alt: 'Interior Project 12' },
+  { src: '/images/interior/IMG_3810.jpg', alt: 'Interior Project 13' },
+  { src: '/images/interior/IMG_3811.jpg', alt: 'Interior Project 14' },
  
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_3813.JPG', alt: 'Interior Project 16' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4165.JPG', alt: 'Interior Project 17' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4299.JPG', alt: 'Interior Project 18' },
+  { src: '/images/interior/IMG_3813.jpg', alt: 'Interior Project 16' },
+  { src: '/images/interior/IMG_4165.jpg', alt: 'Interior Project 17' },
+  { src: '/images/interior/IMG_4299.jpg', alt: 'Interior Project 18' },
 
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4302.JPG', alt: 'Interior Project 20' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4321.JPG', alt: 'Interior Project 21' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/interior/IMG_4655.JPG', alt: 'Interior Project 22' },
+  { src: '/images/interior/IMG_4302.jpg', alt: 'Interior Project 20' },
+  { src: '/images/interior/IMG_4321.jpg', alt: 'Interior Project 21' },
+  { src: '/images/interior/IMG_4655.jpg', alt: 'Interior Project 22' },
  
 ]
 
 const exteriorProjects = [
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4352.JPG', alt: 'Exterior Project 1' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4353.JPG', alt: 'Exterior Project 2' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4354.JPG', alt: 'Exterior Project 3' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4356.JPG', alt: 'Exterior Project 4' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4357.JPG', alt: 'Exterior Project 5' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4359.JPG', alt: 'Exterior Project 6' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4361.JPG', alt: 'Exterior Project 7' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4368.JPG', alt: 'Exterior Project 8' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4382.JPG', alt: 'Exterior Project 9' },
-  { src: 'https://trb9yrhq5p76ro9s.public.blob.vercel-storage.com/ex/IMG_4485.JPG', alt: 'Exterior Project 10' },
+  { src: '/images/exterior/IMG_4352.jpg', alt: 'Exterior Project 1' },
+  { src: '/images/exterior/IMG_4353.jpg', alt: 'Exterior Project 2' },
+  { src: '/images/exterior/IMG_4354.jpg', alt: 'Exterior Project 3' },
+  { src: '/images/exterior/IMG_4356.jpg', alt: 'Exterior Project 4' },
+  { src: '/images/exterior/IMG_4357.jpg', alt: 'Exterior Project 5' },
+  { src: '/images/exterior/IMG_4359.jpg', alt: 'Exterior Project 6' },
+  { src: '/images/exterior/IMG_4361.jpg', alt: 'Exterior Project 7' },
+  { src: '/images/exterior/IMG_4368.jpg', alt: 'Exterior Project 8' },
+  { src: '/images/exterior/IMG_4382.jpg', alt: 'Exterior Project 9' },
+  { src: '/images/exterior/IMG_4485.jpg', alt: 'Exterior Project 10' },
  
 ]
 
 export default function Work() {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    setImagesLoaded(true);
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
@@ -58,13 +65,16 @@ export default function Work() {
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-12">
         {interiorProjects.map((project, index) => (
           <div key={index} className="relative w-full h-48">
-            <CustomImage 
-              src={project.src} 
-              alt={project.alt} 
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-            />
+            {imagesLoaded && (
+              <Image 
+                src={project.src} 
+                alt={project.alt} 
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                loading="lazy"
+              />
+            )}
           </div>
         ))}
       </div>
@@ -77,13 +87,16 @@ export default function Work() {
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {exteriorProjects.map((project, index) => (
           <div key={index} className="relative w-full h-48">
-            <CustomImage 
-              src={project.src} 
-              alt={project.alt} 
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-            />
+            {imagesLoaded && (
+              <Image 
+                src={project.src} 
+                alt={project.alt} 
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+                loading="lazy"
+              />
+            )}
           </div>
         ))}
       </div>
