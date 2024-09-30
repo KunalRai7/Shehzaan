a63c03ad86ddea293c16bc051d537efc64a22095
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-09-30 17:11:35 +0530

    pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Shehzaan Luxury Furniture',
-  description: 'Exquisite Furniture & Interior Design',
+  title: 'Shehzaan  ',
+  description: 'Interior Design',
 }
 
 export default function RootLayout({
@@ -11,8 +11,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const basePath = process.env.NODE_ENV === 'production' ? '/Shehzaan' : '';
+
   return (
     <html lang="en">
+      <head>
+        <base href={basePath + '/'} />
+      </head>
       <body>{children}</body>
     </html>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     domains: ['trb9yrhq5p76ro9s.public.blob.vercel-storage.com'],
   },
+  output: 'export',
+  basePath: '/Shehzaan',
 };
 
 export default nextConfig;
