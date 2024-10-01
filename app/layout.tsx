1ee56745012638a7e742ef7481bb7f3f2027937d
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-10-01 13:14:10 +0530

    google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 export const metadata: Metadata = {
   title: 'Shehzaan  ',
@@ -12,13 +13,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const basePath = process.env.NODE_ENV === 'production' ? '/Shehzaan' : '';
+  const GA_MEASUREMENT_ID = 'G-8GF0Z0WF7Y';
 
   return (
     <html lang="en">
       <head>
         <base href={basePath + '/'} />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <GoogleAnalytics GA_MEASUREMENT_ID={GA_MEASUREMENT_ID} />
+      </body>
     </html>
   )
 }
