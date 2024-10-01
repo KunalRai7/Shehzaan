5e0728a93f547c162a0e35f0cdd2f66d83387bc2
KunalRai7 <oneplus.kunal.rai@gmail.com>
2024-10-01 13:18:35 +0530

    0001

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,45 +1,22 @@
-'use client';
+import Script from 'next/script'
+const GA_MEASUREMENT_ID = 'G-8GF0Z0WF7Y';
 
-import Script from 'next/script';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-
-// Declare gtag as a global function
-declare global {
-  interface Window {
-    gtag: (command: string, ...args: any[]) => void;
-  }
-}
-
-export default function GoogleAnalytics({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_ID: string }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const url = pathname + searchParams.toString();
-    window.gtag('config', GA_MEASUREMENT_ID, {
-      page_path: url,
-    });
-  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
-
+export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}');
-          `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </>
-  );
+  )
 }
